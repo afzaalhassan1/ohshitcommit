de541fae3be17e0b260de6c20d508115c8b399e1
Afzaal <syedafzaal.hassan3@gmail.com>
2014-05-03 15:04:21 -0400

    added table and data in table

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,33 @@
-hjbhjbbj
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <table class="table table-striped table-condensed table-hover">
+        <thead>
+          <tr>
+            <th>Commit message</th>
+            <th>Project name</th>
+            <th>Project description</th>
+            <th>Ratings</th>
+            <th>Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>I have no f$#^in idea what I am doing!"</td>
+            <td>Making first website</td>
+            <td>I was making my first website using PHP</td>
+            <td>5 rotten tomatoes</td>
+            <td>Afzaal</td>
+          </tr>
+          <tr>
+            <td>I have no f$#^in what i am doing!"</td>
+            <td>Making first website</td>
+            <td>I was making my first website using PHP</td>
+            <td>5 rotten tomatoes</td>
+            <td>Afzaal</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
