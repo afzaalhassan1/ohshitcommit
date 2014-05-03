bb87516bff5fad832d3c76303dfb4dcc0ffd8747
Afzaal <syedafzaal.hassan3@gmail.com>
2014-05-03 18:48:15 -0400

    Preliminary webpage features

--- application/views/author_v.php
+++ application/views/author_v.php
@@ -1,13 +1,44 @@
 <div class="container-fluid">
   <div class="row">
+    <div class="col-md-12">
+      <div class="alert alert-success alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        This is sooo right!
+      </div>
+    </div>
+  </div>
+  <div class="row">
     <div class="col-md-3">
       <img src="http://placehold.it/250x250&text=PROFILE PIC">
       <br />
       <strong>Name:</strong> My name<br />
-      
+      <div class="row">
+        <div class="col-md-6"> 
+          FIRST
+        </div>
+        <div class="col-md-6"> 
+          SECOND
+        </div>
+      </div>
     </div>
     <div class="col-md-9">
-      NINE!!!!
+      <div class="row">
+        <div class="col-md-6">
+          <div class="well well-sm">
+            <h3>My repositories</h3>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              Top dumb commit
+            </div>
+            <div class="panel-body">
+              "LOL, this shit dumb"
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
