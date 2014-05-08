e5a69d7ed3ef33f09a89e118b6f013338a5a40af
Afzaal <syedafzaal.hassan3@gmail.com>
2014-05-07 20:09:07 -0400

    Got rid of extra search bar

--- application/views/author_v.php
+++ application/views/author_v.php
@@ -57,12 +57,4 @@
       <input type="text" class="form-control">
     </div><!-- /input-group -->
   </div><!-- /.col-lg-6 -->
-  <div class="col-lg-6">
-    <div class="input-group">
-      <input type="text" class="form-control">
-      <span class="input-group-btn">
-        <button class="btn btn-default" type="button">Go!</button>
-      </span>
-    </div><!-- /input-group -->
-  </div><!-- /.col-lg-6 -->
 </div><!-- /.row -->
