ebbc50f4352d6ab81474ef75088994d4c9965720
Afzaal <syedafzaal.hassan3@gmail.com>
2014-05-17 13:47:48 -0400

    Fixed all indentation in the view webpage

--- application/views/author_v.php
+++ application/views/author_v.php
@@ -5,7 +5,7 @@
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
         This is sooo right!
       </div>
-        <h3>My awesome picture!</h3>
+      <h3>My awesome picture!</h3>
     </div>
   </div>
   <div class="row">
@@ -25,13 +25,11 @@
     <div class="col-md-9">
       <div class="row">
         <div class="col-md-6">
-          <table class="table table-bordered">
-            <div class="well well-sm">
-              <p class="bg-primary">
-                <h3>My repositories</h3>
-              </p>
-            </div>
-          </table>
+          <div class="well well-sm">
+            <p class="bg-primary">
+              <h3>My repositories</h3>
+            </p>
+          </div>
         </div>
         <div class="col-md-6">
           <div class="panel panel-default">
@@ -45,19 +43,19 @@
       </div>
     </div>
   </div>
-</div>
-<div class="row">
-  <div class="col-lg-6">
-    <div class="input-group">
-      <span class="input-group-btn">
-        <button class="btn btn-default" type="button">Search!</button>
-      </span>
-      <input type="text" class="form-control">
-    </div><!-- /input-group -->
-    <div class="progress progress-striped active">
-      <div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
-        <span class="sr-only">50% Complete</span>
+  <div class="row">
+    <div class="col-lg-6">
+      <div class="input-group">
+        <span class="input-group-btn">
+          <button class="btn btn-default" type="button">Search!</button>
+        </span>
+        <input type="text" class="form-control">
+      </div><!-- /input-group -->
+      <div class="progress progress-striped active">
+        <div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+          <span class="sr-only">50% Complete</span>
+        </div>
       </div>
-    </div>
-  </div><!-- /.col-lg-6 -->
-</div><!-- /.row -->
+    </div><!-- /.col-lg-6 -->
+  </div><!-- /.row -->
+</div>
