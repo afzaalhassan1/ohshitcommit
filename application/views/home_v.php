d5c39414d5dbcb6ba09bad8bc5e4d7652988f7c8
Afzaal <syedafzaal.hassan3@gmail.com>
2014-05-17 13:36:46 -0400

    Fixed all indentation in the home view page

--- application/views/home_v.php
+++ application/views/home_v.php
@@ -27,50 +27,58 @@
             <td>I was making my first website using PHP</td>
             <td>5 rotten tomatoes</td>
             <td>Afzaal</td>
-          </tr>        
-          <div class="row">
-           <div class="col-lg-6">
-            <div class="dropdown">
-              <button class="btn dropdown-toggle sr-only" type="button" id="dropdownMenu1" data-toggle="dropdown">
-                Dropdown
-                <span class="caret"></span>
-                  </button>
-                  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Search</a></li>
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Search keywords</a></li>
-                  <li role="presentation" class="divider"></li>
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-                </ul>
-                </div>
-                </div>
-          </div>
-        </tbody>
+          </tr>  
+        </tbody>      
       </table>
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="dropdown">
+            <button class="btn dropdown-toggle sr-only" type="button" id="dropdownMenu1" data-toggle="dropdown">
+              Dropdown
+              <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Search</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Search keywords</a></li>
+              <li role="presentation" class="divider"></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-9">
+      <div class="panel panel-default">
+        <div class="panel-heading"> Username table</div>
+        <div class="panel-body">
+          "Username"
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Comment List</h3>
+        </div>
+        <div class="panel-body">
+          "Comments"
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+          <span class="sr-only">60% Complete</span>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-md-offset-3">
     </div>
   </div>
 </div>
-          <div class = "row">
-            <div class = "col-lg-9">
-              <div class="panel panel-default">
-                <div class="panel-heading"> Username table</div>
-                  <div class="panel-body">
-                  "Username"
-                </div>
-                </div>
-
-                  <div class="panel panel-default">
-                    <div class="panel-heading">
-                      <h3 class="panel-title">Comment List</h3>
-                </div>
-                  <div class="panel-body">
-                "Comments"
-                </div>
-                </div>
-                <div class = "row">
-                <div class "col-lg-6">
-                <a href="#">   New commits <span class="badge">2</span></a>
-                <div class="progress">
-                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                  <span class="sr-only">60% Complete</span>
-                  </div>
-                </div>
