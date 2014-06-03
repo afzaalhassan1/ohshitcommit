d633c31c1fb29c101af8d6aa4040716870ec005e
Afzaal <syedafzaal.hassan3@gmail.com>
2014-06-02 22:00:08 -0400

    Fixed table in comment webpage

--- application/views/comment_v.php
+++ application/views/comment_v.php
@@ -35,7 +35,6 @@
               <td><a href="/commit/show/<?php echo $single_comment['id']; ?>"><?php echo $single_comment['comment']; ?></a> </td>
               <td><?php echo $single_comment['author']; ?></td>
               <td><?php echo $single_comment['vote']; ?></td>
-              <td><?php echo $single_comment['comment']; ?></td>
               <td><?php echo $single_comment['comment_date']; ?></td>
             </tr>
           <?php endforeach; ?>
