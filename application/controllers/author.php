8cc98f34dc2d100006cff5ea1bc15be5ec157e1b
Afzaal <syedafzaal.hassan3@gmail.com>
2014-06-13 21:06:38 -0400

    Made the author name appear in jumbotron heading. Made two tables listing the commits and comments for each author

--- application/controllers/author.php
+++ application/controllers/author.php
@@ -18,10 +18,11 @@ class Author extends CI_Controller {
 
     $commits = $this->Commit_m->get_by_name($author_name);
     $comments = $this->Comment_m->get_by_author($author_name);
-
+    
     
     $this->template->set('commits', $commits);
     $this->template->set('comments', $comments);
+    $this->template->set('authorname', $author_name);
     $this->template->build('author_v');
   }
 }
--- application/views/author_v.php
+++ application/views/author_v.php
@@ -2,27 +2,36 @@
   <div class="row">
     <div class="col-md-12">
       <div class="jumbotron">
-        <h1><?php echo $commits['name']; ?></h1>
+        <h1><?php echo $authorname; ?></h1>
       </div>
     </div>
   </div>
   <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-6">
       <table class="table table-striped table-condensed table-hover">
         <thead>
           <tr>
             <th>Commit Message</th>
-            <th>Comment Message</th>
           </tr>
         </thead>
         <tbody>
           <?php foreach ($commits as $commit): ?>
             <tr>
-              <td><?php echo $commit['message']; ?> </td>
+              <td><?php echo $commit['message']; ?></td>
             </tr>
           <?php endforeach; ?>
-          <?php foreach ($comments as $comment): ?>
-            <tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="col-md-6">
+      <table class="table table-striped table-condensed table-hover">
+        <thead>
+          <tr>
+            <th>Comment Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($comments as $comment): ?> 
               <td><?php echo $comment['comment']; ?> </td>
             </tr>
           <?php endforeach; ?>
